dbd4a37f06eb1367eed4aab0041babc422d6a5cc
Heshi4 <heshani03123@gmail.com>
2024-05-16 17:52:12 +0530

    feedback_reviews

---
+++ src/components/ReviewPops.js
@@ -0,0 +1,159 @@
+import React, { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FiXCircle } from "react-icons/fi";
+import axios from 'axios';
+
+
+
+function ReviewPops(props) {
+
+
+  const[category, setCategory] = useState('');
+  const[comments, setComments] = useState('');
+  const[errorMessage, setErrorMessage] = useState('');
+  const navigate=useNavigate();
+  
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+    console.log(props.closePop)
+  };
+
+  const handleCommentsChange = (event) => {
+    setComments(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Reset error message
+    setErrorMessage('');
+
+
+    if(!category.trim()&& !comments.trim()){
+      setErrorMessage('Category and comment cannot be empty');
+      return; // Exit the function early
+    }
+
+
+    // Check if category and comments are empty
+    if (!category.trim()) {
+      setErrorMessage('Category cannot be empty');
+      return; // Exit the function early
+    }
+
+    if (!comments.trim()) {
+      setErrorMessage('Comments cannot be empty');
+      return; // Exit the function early
+    }
+
+  
+    // If no errors, proceed with form submission
+    axios.post('http://localhost:5000/review/reviewAdd', {
+      review: comments,
+      reviewCatagory: category,
+      reviewUser: 'shanu@gmail.com',
+      productID: '001'
+    })
+    .then((response) => {
+      console.log("Data sent successfully:", response.data);
+      // Optionally navigate to another page after successful submission
+      navigate('/comments')
+    })
+    .catch((error) => {
+      console.error("Error occurred while sending data:", error);
+    });
+  };
+  /// update review function......./////
+
+  const updateReview = (event) => {
+   event.preventDefault();
+    
+    // Reset error message
+    setErrorMessage('');
+
+
+    if(!category.trim()&& !comments.trim()){
+      setErrorMessage('Category and comment cannot be empty');
+      return; // Exit the function early
+    }
+
+
+    // Check if category and comments are empty
+    if (!category.trim()) {
+      setErrorMessage('Category cannot be empty');
+      return; // Exit the function early
+    }
+
+    if (!comments.trim()) {
+      setErrorMessage('Comments cannot be empty');
+      return; // Exit the function early
+    }
+
+
+        axios.put('http://localhost:5000/review/reviewUpdate',{
+            data:{
+              reviewId:props.data,
+              review:comments,
+              reviewCatagory:category,
+            }
+        })
+        .then((response) => {
+          console.log("Data sent successfully:", response.data);
+           navigate('/comments')
+        })
+        .catch((error) => {
+          console.error("Error occurred while sending data:", error);
+          
+        });
+    
+};
+
+  return (
+    <div className="border border-gray-300 p-4 rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white w-700 h-200">
+      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <h2 className="text-lg  mt-4 font-bold mb-4">Review</h2>
+        <button onClick={props.closePop} >
+              <i>< FiXCircle size={25} className=" mr-2 ml-1 mt-1  hover:scale-75 duration-300"/></i>
+           </button>
+      </div>
+      {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
+      <form onSubmit={ (props.buttonName==="Update")?updateReview:handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="category" className="block text-sm font-medium text-gray-700">
+            Category
+          </label>
+          <select
+            id="category"
+            value={category}
+            onChange={handleCategoryChange}
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          > 
+             <option value="" disabled hidden>Comments category</option>
+             <option value="Bad Review">Bad Review</option>
+             <option value="Good Review">Good Review</option>
+            
+          </select>
+           
+        </div>
+        <div className="mb-4">
+          <label htmlFor="comments" className="block text-sm font-medium text-gray-700">
+            Comments
+          </label>
+          <textarea
+            id="comments"
+            value={comments}
+            onChange={handleCommentsChange}
+            rows="3"
+            className="mt-1 p-2 border border-gray-300 rounded-md w-full"
+          ></textarea>
+        </div>
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          {props.buttonName}
+        </button>
+      </form>
+    </div>
+  );
+}
+ 
+export default ReviewPops;
---
+++ src/components/ViewReview.js
@@ -0,0 +1,61 @@
+import React,{useState,useEffect} from "react";
+import avatar from '../../assets/avatar.jpg';
+import { FiXCircle } from "react-icons/fi";
+import ReviewPops from "./ReviewPops";
+import axios from 'axios';
+
+
+
+export default function ViewReview(props){
+
+const [propsType,setpropType]=useState(false);
+
+
+
+
+const editReview=()=>{
+    setpropType(true)
+}
+
+const close=()=>{
+    console.log("hiiiiiiiiiiiiiiii")
+    setpropType(false)
+}
+
+
+
+ return(
+    <div style={{height:'200px',width:'400px',boxShadow: "1px 3px 1px #9E9E9E",borderRadius:'10px',outline:'2px solid Black',marginBottom: '40px'}}>
+    <div style={{height:'50px',display:'flex',flexDirection:'row',justifyContent:'space-evenly',alignItems:'center',padding:'3px'}}>
+        <div style={{height:'45px',width:'45px',borderRadius:'45px'}}>
+            <img src={avatar}></img>
+        </div>
+        <div style={{height:'45px',width:'280px',color:'black',display:'flex', flexDirection: 'column'}}>
+            
+            <div>{props.data.reviewUser}</div>
+            <div>{props.data.reviewCatagory}</div> 
+            
+            </div>
+        <div  style={{height:'45px',width:'45px'}}>
+           <button onClick={()=>props.delete(props.data._id)}>
+              <i>< FiXCircle size={35} className=" mr-2 ml-1 mt-1  hover:scale-75 duration-300"/></i>
+           </button>
+        </div>
+    </div>
+    <div style={{height:'2px',width:'100%',paddingLeft:'5%',paddingRight:'5%'}}>
+        <div style={{width:'98%',height:'1px',backgroundColor:'black'}}></div>
+        </div>
+    <div style={{width:'100%',height:'100px',overflowY:'auto',wordWrap:'break-word',padding:'16px'}}>{props.data.review}</div>
+    <div style={{height:'48px',width:'100%',display:'flex',alignItems:'center',justifyContent:'space-around'}}>
+        <button onClick={editReview} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          Edit
+        </button>
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+          More
+        </button>
+    </div>
+    {propsType&&<ReviewPops closePop={close} buttonName="Update" data={props.data._id}/>}
+ </div>
+ )
+    
+}
