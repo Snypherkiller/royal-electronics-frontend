25c12be58eb6f19a096a9338e2df01fb333cc704
Heshi4 <heshani03123@gmail.com>
2024-05-16 17:50:17 +0530

    feedback_navbars

---
+++ src/components/Card_coustom.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/logo.jpg";
+import fiture from "../assets/f1.jpeg"
+
+
+
+   
+  export default function CardCoustom(props) {
+    return (
+          <div class="max-w-sm   mt-10 ml-6 mb-5 bg-white border  hover:mt-5 duration-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <img src={fiture} alt="alat" className=" rounded-t-lg  w-fit "/>  
+                 <hr></hr>                
+              <div class="p-5">
+                  <a href="#">
+                  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Item</h5>
+
+                  </a>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Information</p>
+                  <button  onClick={props.eventFunction} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    {props.buttonName}
+                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                  </button>
+              </div>
+          </div>
+
+    );
+  }
---
+++ src/components/SiderBar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import { FaUserGraduate } from "react-icons/fa";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { MdOutlineRateReview } from "react-icons/md";
+import { RiShoppingCartLine } from "react-icons/ri";
+import { BsBoxArrowUpLeft } from "react-icons/bs";
+
+
+export default function SiderBar(){
+ return(
+      <div className="bg-gray-800 text-white w-64">
+        <div className="p-4">
+          <div className="flex">
+            <i><FaUserGraduate size={30} className=" ml-6"/></i>
+            <h1 className="text-xl font-bold mb-4 ml-4">Heshani </h1>
+          </div>
+          <hr />
+          <ul>
+            <li className="mb-2   ml-0 hover:scale-50  duration-500">
+              <Link to="/" className="block px-4 py-2 rounded hover:bg-gray-700 mt-6 font-extrabold  flex"> <MdOutlineDashboardCustomize size={35} className=" mr-4"/> Dashboard</Link>
+            </li>
+            <li className="mb-2  ml-0 hover:scale-50  duration-500">
+              <Link to="/comments" className="block px-4 py-2 rounded hover:bg-gray-700  mt-6 font-extrabold flex" ><MdOutlineRateReview  size={35} className=" mr-4"/>Review</Link>
+            </li>
+            <li className="mb-2 hover:scale-50  duration-500">
+              <Link to="/Puchers" className="block px-4 py-2 rounded hover:bg-gray-700 mt-6 font-extrabold flex"><RiShoppingCartLine  size={35} className=" mr-4"/>Purchased Item</Link>
+            </li>
+            <li className=" mt-40  ml-0 hover:scale-50  duration-500 ">
+              <Link to="#" className=" flex font-extrabold block px-4 py-2 rounded  hover:bg-gray-700 "><BsBoxArrowUpLeft size={25} className=" mr-5 ml-4"/>log out</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      
+   
+ );
+}
